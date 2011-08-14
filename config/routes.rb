fc37cc58770b06597ea19e4d1997ab9d9045c8db
Sergey Karpesh <sergey.karpesh@gmail.com>
2011-08-14 20:10:24 +0300

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 SampleApp::Application.routes.draw do
+
+  get "users/new"
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+  get "pages/help"
+
+  match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
 
   root :to => 'pages#home'
-#  get "pages/home"
-#  get "pages/contact"
-#  get "pages/about"
-#  get "pages/help"
+
 end
 
