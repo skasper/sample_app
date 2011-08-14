597598522fbc149684e35ed01beebed2768abef2
Sergey Karpesh <sergey.karpesh@gmail.com>
2011-08-14 15:14:01 +0300

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -10,5 +10,6 @@ end
 group :test do
   gem 'rspec-rails', '2.6.1'
   gem 'webrat'
+  gem 'spork', '0.9.0.rc8'
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'spork'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -25,3 +27,7 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 end
+
+Spork.each_run do
+end
+
