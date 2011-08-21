58630165626183bd174c94a1849fc23c7bb97562
Sergey Karpesh <sergey.karpesh@gmail.com>
2011-08-21 21:58:24 +0300

    User model with passwords and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,19 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
+require "digest"
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -19,5 +23,38 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  def self.authenticate(email, submited_password)
+    user = User.find_by_email(email)
+    user if !user.nil? && user.has_password?(submited_password)
+  end
+
+  def  has_password?(submited_password)
+    encrypted_password == encrypt(submited_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
 
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+  user.name                  "Sergey Karpesh"
+  user.email                 "kasper@gmail.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
+
